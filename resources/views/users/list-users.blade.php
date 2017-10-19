59ff278df42d599677c528e7e3af0415bcc5110d
Marek Schauer <marek.schauer@psoit.sk>
2017-10-19 12:48:45 +0000

    First commit

---
+++ resources/views/users/list-users.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="col-md-12">
+	<h1>Users</h1>
+
+	<!-- Table HTML -->
+	<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Email</th>
+				<th>Created at</th>
+				<th>Updated at</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<th>Name</th>
+				<th>Email</th>
+				<th>Created at</th>
+				<th>Updated at</th>
+				<th>Actions</th>
+			</tr>
+		</tfoot>
+		<tbody>
+			@foreach($users as $user)
+			<tr>
+				<td>{{ $user->name }}</td>
+				<td>{{ $user->email }}</td>
+				<td>{{ $user->created_at }}</td>
+				<td>{{ $user->updated_at }}</td>
+				<td>X|X|X</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
+@endsection
+
+@section('scripts')
+
+<script>
+	   // Using aoColumnDefs
+	   $(document).ready( function() {
+	   	$('#example').dataTable( {
+	   		"aoColumnDefs": [ 
+	   		{ "bSortable": false, "aTargets": [ 0 ] }
+	   		] } );
+	   } );
+
+
+   // Using aoColumns
+   $(document).ready( function() {
+   	$('#example').dataTable( {
+   		"aoColumns": [ 
+   		null,
+   		null,
+   		null,
+   		null,
+   		{ "bSortable": false }
+   		] } );
+   } );
+</script>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+// Route::get('/{pattern}/{example}', function ($pattern, $example) {
+//     return view('patternfly.'.$pattern.'.'.$example);
+// });
+
+Route::get('/home', 'HomeController@index');
+
+Auth::routes();
+
+Route::get('/users', 'UserController@index')->name('users');
+Route::get('/user/{id}', 'UserController@show');
