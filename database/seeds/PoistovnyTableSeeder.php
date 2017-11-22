0d49c31c894c1749082e60d9cbe5c3d04dd538e5
Adrián Tóth <adricko28@gmail.com>
2017-11-22 12:06:20 +0100

    data added to PoistovnyTableSeeder

--- database/seeds/PoistovnyTableSeeder.php
+++ database/seeds/PoistovnyTableSeeder.php
@@ -11,9 +11,14 @@ class PoistovnyTableSeeder extends Seeder
      */
     public function run()
     {
-        //
         DB::table('pojistovny')->insert(array(
-    		array('nazev_pojistovny' => 'Addaven'),
-    		));
+            array('nazev_pojistovny' => 'Vseobecna zdravotni pojistovna CR'),
+            array('nazev_pojistovny' => 'Vojenska zdravotni pojistovna CR'),
+            array('nazev_pojistovny' => 'Ceska prumyslova zdravotni pojistovna'),
+            array('nazev_pojistovny' => 'Oborova zdravotni poj. zam. bank, poj. a stav.'),
+            array('nazev_pojistovny' => 'Zamestnanecka pojistovna Skoda'),
+            array('nazev_pojistovny' => 'Zdravotni pojistovna ministerstva vnitra CR'),
+            array('nazev_pojistovny' => 'Revirni bratrska pokladna, zdrav. pojistovna'),
+            ));
     }
 }
