89a70d8959409a9fe7e422a3817e279fc4912dfc
Adrián Tóth <adricko28@gmail.com>
2017-11-24 14:54:24 +0100

    authors

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -31,9 +31,32 @@
                             </button>
                         </div>
                         <div class="modal-body">
-                            <h1><strong>{{config('app.name')}}</strong></h1>
-                                 <div class="trademark-pf">
-                                <h3>&copy; 2017 Schauer Marek, Šuhaj Peter, Tóth Adrián</h2>
+                            <h1>
+                                <strong>{{config('app.name')}}</strong>
+                                <br><br> <!-- HACK -->
+                            </h1>
+                            <table class="table text-center">
+                                <tbody>
+                                    <tr>
+                                        <td><strong>Schauer</strong></td>
+                                        <td>Marek</td>
+                                        <td><a class="text-right" href="mailto:xschau00@stud.fit.vutbr.cz">xschau00@stud.fit.vutbr.cz</a></td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>Šuhaj</strong></td>
+                                        <td>Peter</td>
+                                        <td><a class="text-right" href="mailto:xsuahj02@stud.fit.vutbr.cz">xsuahj02@stud.fit.vutbr.cz</a></td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>Tóth</strong></td>
+                                        <td>Adrián</td>
+                                        <td><a class="text-right" href="mailto:xtotha01@stud.fit.vutbr.cz">xtotha01@stud.fit.vutbr.cz</a></td>
+                                    </tr>
+                                    <tr><td></td><td></td><td></td></tr> <!-- HACK -->
+                                </tbody>
+                            </table>
+                            <div class="trademark-pf">
+                                <h3><br>&copy; 2017 Schauer Marek, Šuhaj Peter, Tóth Adrián</h3>
                             </div>
                         </div>
                         <div class="modal-footer">
