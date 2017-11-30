a86cd9c611b6ffa3e8f688eaefca4a98b1007256
Adrián Tóth <adricko28@gmail.com>
2017-11-30 13:55:40 +0100

    FIXED: alert-dismissable

--- resources/views/alerts/status.blade.php
+++ resources/views/alerts/status.blade.php
@@ -1,12 +1,18 @@
 @if (Session::has('status-fail'))
-<div class="alert alert-danger" role="alert">
+<div class="alert alert-danger alert-dismissable" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+		<span class="pficon pficon-close"></span>
+	</button>
 	<span class="pficon pficon-error-circle-o"></span>
 	{!!Session::get('status-fail')!!}
 </div>
 @endif
 
 @if (Session::has('status-success'))
-<div class="alert alert-success" role="alert">
+<div class="alert alert-success alert-dismissable" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+		<span class="pficon pficon-close"></span>
+	</button>
 	<span class="pficon pficon-ok"></span>
 	{!!Session::get('status-success')!!}
 </div>
