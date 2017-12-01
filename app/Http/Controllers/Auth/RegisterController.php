9efcaf81b76fb020b470ec3e5e365198813047cc
peter2141 <pupet2141@gmail.com>
2017-12-01 22:06:18 +0100

    removed forgot your password btn, fixing adding admin with pobocka

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -88,7 +88,7 @@ class RegisterController extends Controller
                 'name' => 'required|max:255',
                 'email' => 'required|email|max:255|unique:users',
                 'password' => 'required|min:6|confirmed',
-                // 'pobocka' => 'required|in:none'
+                'pobocka' => 'required|in:none'
             ]);
         }
         else {   
