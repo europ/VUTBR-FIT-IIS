481c3d5dd3933b4886f0656e9cd60b75cd52d3d5
peter2141 <pupet2141@gmail.com>
2017-12-04 19:35:49 +0100

    removed comments from databaseseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('LekyTableSeeder');
-        // $this->call('DodavateleTableSeeder');
-        // $this->call('PobockyTableSeeder');
-        // $this->call('PobockyLekyTableSeeder');
-        // $this->call('PoistovnyTableSeeder');
-        // $this->call('RezervaceTableSeeder');
-        // $this->call('PredpisyTableSeeder');
-        // $this->call('CenyDodavateluSeeder');
-        // $this->call('PredpisyLekySeeder');
-        $this->call('ProdaneLekySeeder');
+         $this->call('LekyTableSeeder');
+         $this->call('DodavateleTableSeeder');
+         $this->call('PobockyTableSeeder');
+         $this->call('PobockyLekyTableSeeder');
+         $this->call('PoistovnyTableSeeder');
+         $this->call('RezervaceTableSeeder');
+         $this->call('PredpisyTableSeeder');
+         $this->call('CenyDodavateluSeeder');
+         $this->call('PredpisyLekySeeder');
+         $this->call('ProdaneLekySeeder');
+         $this->call('RezervaceLekySeeder');
     }
 }
